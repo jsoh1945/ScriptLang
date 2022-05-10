751551cd4fc44757b70bf8b659a2124d4b52af07
eAg302 <65862680+eAg302@users.noreply.github.com>
2022-05-10 12:20:37 +0900

    cell 생성구문 변경

--- games/4목/4목.py
+++ games/4목/4목.py
@@ -34,13 +34,8 @@ class Cell(Canvas):
         self.color = color
         self.create_oval(4,4,20,20, fill = self.color,tags='oval')
 
-cells = [[Cell(frame1,0,0),Cell(frame1,0,1),Cell(frame1,0,2),Cell(frame1,0,3),Cell(frame1,0,4),Cell(frame1,0,5),Cell(frame1,0,6)],
-        [Cell(frame1,1,0),Cell(frame1,1,1),Cell(frame1,1,2),Cell(frame1,1,3),Cell(frame1,1,4),Cell(frame1,1,5),Cell(frame1,1,6)],
-        [Cell(frame1,2,0),Cell(frame1,2,1),Cell(frame1,2,2),Cell(frame1,2,3),Cell(frame1,2,4),Cell(frame1,2,5),Cell(frame1,2,6)],
-        [Cell(frame1,3,0),Cell(frame1,3,1),Cell(frame1,3,2),Cell(frame1,3,3),Cell(frame1,3,4),Cell(frame1,3,5),Cell(frame1,3,6)],
-        [Cell(frame1,4,0),Cell(frame1,4,1),Cell(frame1,4,2),Cell(frame1,4,3),Cell(frame1,4,4),Cell(frame1,4,5),Cell(frame1,4,6)],
-        [Cell(frame1,5,0),Cell(frame1,5,1),Cell(frame1,5,2),Cell(frame1,5,3),Cell(frame1,5,4),Cell(frame1,5,5),Cell(frame1,5,6)],
-        ]
+
+cells = [[Cell(frame1,r,c) for c in range(7) ] for r in range(6)]
 
 
 for r in range(0,6):
