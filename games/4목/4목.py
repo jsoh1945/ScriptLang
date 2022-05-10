e65d612ce63449ab6dc19d8d167e2958db61238d
오현택 <jsoh1945@naver.com>
2022-05-10 12:57:24 +0900

    Update 4목.py

--- games/4목/4목.py
+++ games/4목/4목.py
@@ -10,13 +10,18 @@ frame2 = Frame(window)
 frame2.pack()
 
 statusLabel = ['새로 시작','Red Win', 'Yellow Win']
-statusIndex = 0
+statusIndex = 0 #아래 버튼의 문구 변경하는 index
+
+Turn = ['red', 'yellow', 'None']
+TurnIndex = 0
+
+ColIndex = [0,0,0,0,0,0,0] #해당 열의 어디까지 돌이 차있는가에 대한 리스트 
 
 class Cell(Canvas):
     def __init__(self,parent,col,row,width=20,height=20):
         Canvas.__init__(self,parent,width=width,height=height,\
             bg = "blue", borderwidth=2)
-        self.color="white"
+        self.color='white'
         self.row = row
         self.col = col
         self.create_oval(4,4,20,20,fill='white',tags='oval')
@@ -29,13 +34,15 @@ class Cell(Canvas):
         return self.row
 
     def clicked(self,event):
-        nextcolor = 'red' if self.color != 'red' else 'yellow'
+        nextcolor =  'red' if self.color != 'red' else 'yellow'
         self.setColor(nextcolor)
+        
 
     def setColor(self,color):
         self.delete('oval')
         self.color = color
         self.create_oval(4,4,20,20, fill = self.color,tags='oval')
+        
 
 def Change():
     global statusIndex
@@ -45,11 +52,10 @@ def Change():
         statusIndex = 0
     statusButton["text"] = statusLabel[statusIndex]
 
+
 cells = [[Cell(frame1,r,c) for c in range(6) ] for r in range(7)]
 statusButton = Button(frame2,text=statusLabel[statusIndex], command=Change)
 statusButton.pack()
-
-
 
 
 
