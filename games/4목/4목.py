284fc746e8e3be6e4bfc5ea7fc0c0adb472e6039
오현택 <jsoh1945@naver.com>
2022-05-10 12:29:52 +0900

    Update 4목.py
    아래 버튼 생성
    Change함수로 클릭할때마다 문구 바뀌도록 설정

--- games/4목/4목.py
+++ games/4목/4목.py
@@ -9,6 +9,9 @@ frame1.pack()
 frame2 = Frame(window)
 frame2.pack()
 
+statusLabel = ['새로 시작','Red Win', 'Yellow Win']
+statusIndex = 0
+
 class Cell(Canvas):
     def __init__(self,parent,col,row,width=20,height=20):
         Canvas.__init__(self,parent,width=width,height=height,\
@@ -34,12 +37,24 @@ class Cell(Canvas):
         self.color = color
         self.create_oval(4,4,20,20, fill = self.color,tags='oval')
 
+def Change():
+    global statusIndex
+    global statusButton
+    statusIndex +=1
+    if(statusIndex == 3):
+        statusIndex = 0
+    statusButton["text"] = statusLabel[statusIndex]
 
-cells = [[Cell(frame1,r,c) for c in range(7) ] for r in range(6)]
+cells = [[Cell(frame1,r,c) for c in range(6) ] for r in range(7)]
+statusButton = Button(frame2,text=statusLabel[statusIndex], command=Change)
+statusButton.pack()
 
 
-for r in range(0,6):
-    for c in range(0,7):
+
+
+
+for r in range(0,7):
+    for c in range(0,6):
         cells[r][c].grid(row=cells[r][c].getRow(),column=cells[r][c].getCol())
 
 
