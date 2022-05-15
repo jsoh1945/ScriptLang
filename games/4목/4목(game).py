a9a217bd9f3444a6640ad51d694ed27605c99b6f
eAg302 <65862680+eAg302@users.noreply.github.com>
2022-05-15 16:11:54 +0900

    4목(game).py 새로시작 전까지 구현

--- games/4목/4목(game).py
+++ games/4목/4목(game).py
@@ -27,6 +27,12 @@ class Cell(Canvas):
     def getRow(self):
         return self.row
 
+    def setCol(self,col):
+        self.col = col
+
+    def setRow(self,row):
+        self.row=row
+
     def getColor(self):
         return self.color
 
@@ -35,20 +41,17 @@ class Cell(Canvas):
         # self.setColor(nextcolor)
         global TurnIndex
         global Turn
+        global statusLabel
+        global cells
 
         if TurnIndex == 42: #돌이 다 차서 종료
+            Change(2)
             return
-
         else:
-            #오류
-            #self.row = isPossibleInsert(self.col)
-            if self.color == 'white':
-                TurnIndex += 1
-                self.setColor(Turn[TurnIndex % 2] )
-                print(getPossibleRow(self.col))
-
-
-
+            TurnIndex += 1
+            cells[getPossibleRow(self.col)][self.col].setColor(Turn[TurnIndex % 2])
+            if isWin(Turn[TurnIndex % 2]):
+                Change(1 + TurnIndex % 2)
 
     def setColor(self, color):
         self.delete('oval')
@@ -58,82 +61,65 @@ class Cell(Canvas):
 
 #돌이 들어갈 수 있는 열 반환
 def getPossibleRow(column_index):
-    for row_index in range(5):
+    for row_index in range(5,-1,-1):
         if cells[row_index][column_index].getColor() == 'white':
             return row_index
 
-
-
 def isWin(color):
     #수평
-    for column_index in range(7 - 3):
-        for row_index in range(6):
+    for row_index in range(6):
+        for column_index in range(7 - 3):
             if cells[row_index][column_index].getColor() == color and cells[row_index][column_index + 1].getColor() == color and \
                     cells[row_index][column_index + 2].getColor() == color and cells[row_index][column_index + 3].getColor() == color:
                 return True
     #수직
-    for column_index in range(7):
-        for row_index in range(6 - 3):
+    for row_index in range(6 - 3):
+        for column_index in range(7):
             if cells[row_index][column_index].getColor() == color and cells[row_index + 1][column_index].getColor() == color and \
                     cells[row_index + 2][column_index].getColor() == color and cells[row_index + 3][column_index].getColor() == color:
                 return True
     #대각선 /
-    for column_index in range(7 - 3):
-        for row_index in range(6 - 3):
+    for row_index in range(6 - 3):
+        for column_index in range(7 - 3):
             if cells[row_index][column_index].getColor() == color and cells[row_index + 1][column_index + 1].getColor() == color and \
                     cells[row_index + 2][column_index + 2].getColor() == color and cells[row_index + 3][column_index + 3].getColor() == color:
                 return True
     #대각선 \
-    for column_index in range(7 - 3):
-        for row_index in range(3, 6):
+    for row_index in range(3, 6):
+        for column_index in range(7 - 3):
             if cells[row_index][column_index].getColor() == color and cells[row_index - 1][column_index + 1].getColor() == color and \
                     cells[row_index - 2][column_index + 2].getColor() == color and cells[row_index - 3][column_index + 3].getColor() == color:
                 return True
 
 
-
-
-
-def Change():   # 하단 상태창 수정
-    global statusIndex
+def Change(Index):   # 하단 상태창 수정
+    #global statusIndex
     global statusButton
-    statusIndex += 1
-    if (statusIndex == 3):
-        statusIndex = 0
-    statusButton["text"] = statusLabel[statusIndex]
+    statusButton["text"] = statusLabel[Index]
 
-# def isCellsFull(count):
-#     if count == 42:
-#         return True
-#     return  False
 
+def Initialize():   # 새로시작시 모든 값초기화
+    global  cells
+    global  TurnIndex
+    for r in range(6):
+        for c in range(7):
+            cells[r][c].setColor('white')
+    TurnIndex = 1
 
 Turn = ['red', 'yellow', 'None'] #None : 게임 끝일때.
 TurnIndex = 1
 currentToken = ['white', 'red', 'yellow']         # 돌 상태값
-
-game_finished = False           # 게임이 끝났는지 알려주는 bool변수
-
-#ColIndex = [0, 0, 0, 0, 0, 0, 0]  # 해당 열의 어디까지 돌이 차있는가에 대한 리스트
-columnIndex = 0
-
-
 statusLabel = ['새로 시작', 'Red Win', 'Yellow Win']
-statusIndex = 0  # 아래 버튼의 문구 변경하는 index
+statusIndex = 0 #아래 버튼의 문구 변경하는 index
 
+cells = [[Cell(frame1, row=r, col=c) for c in range(7)] for r in range(6)]
 
-cells = [[Cell(frame1, col=c, row=r) for r in range(6)] for c in range(7)]
-
-
-statusButton = Button(frame2, text=statusLabel[statusIndex], command=Change)
+statusButton = Button(frame2, text=statusLabel[0], command=Change)
 statusButton.pack()
 
-
-
-for r in range(0, 7):
-    for c in range(0, 6):
+for r in range(6):
+    for c in range(7):
         cells[r][c].grid(row=cells[r][c].getRow(), column=cells[r][c].getCol())
-        pass
 
 window.mainloop()
 
