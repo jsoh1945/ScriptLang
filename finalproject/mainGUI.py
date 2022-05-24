2dbb340551bd443cd5fc7d88cc8dbba8e857b15c
오현택 <jsoh1945@naver.com>
2022-05-24 11:37:43 +0900

    GUI Update
    이메일 이미지 추가, 사이트 리다이렉션 구현

--- finalproject/mainGUI.py
+++ finalproject/mainGUI.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from tkinter import font
 from tkinter import ttk
+from urllib.request import urlopen
+import webbrowser #URL 리다이렉션을 위한 import
+
 
 window = Tk() #Create a window
 window.title("코비디피아")  # Set title
@@ -13,6 +16,10 @@ fontTitle = font.Font(window,size=18,weight='bold')
 fontNormal = font.Font(window,size=15,weight='bold')
 ##########################################################################
 
+def urlOpen():
+    webbrowser.open("http://ncov.mohw.go.kr/")
+
+
 #1번째 - 로고, 이메일, 리다이렉션 버튼
 ##########################################################################
 TopFrame = Frame(window,width=600,height=50,background='#00FF00')
@@ -22,10 +29,11 @@ TopFrame.pack(side='top', fill='x')
 titleText = Label(TopFrame,text='코비디피아', font=fontTitle,width=20,height=3,borderwidth=12,relief='ridge')
 titleText.grid(row=0,column=0)
 
-emailButton = Button(TopFrame, text='이메일',width=17,height=6,anchor='center',padx=5,pady=5)
+emailImage = PhotoImage(file="email.png") # 이메일 이미지 추가
+emailButton = Button(TopFrame, image=emailImage,padx=5,pady=5) # 이메일 버튼에 이미지 심어놓음
 emailButton.grid(row=0,column=1)
 
-redirectButton = Button(TopFrame,text='리다이렉션',width=17,height=6,anchor='center',padx=5,pady=5)
+redirectButton = Button(TopFrame,text='리다이렉션',width=17,height=6,anchor='center',padx=5,pady=5,command=urlOpen)
 redirectButton.grid(row=0,column=2)
 ##########################################################################
 
@@ -83,16 +91,29 @@ InfoListBox = Listbox(MapAndListFrame, selectmode='extended',\
 InfoListBox.grid(row=0,column=1)
 ##########################################################################
 
+#5월24일 증상/대처 버튼 클릭 시 텍스트 바뀌게하는 함수
+def SymptomHandleTextChange():
+    global SymptomText
+    global SymptomFlag
+    if SymptomFlag == 0:
+        SymptomText["text"] = "이건 대처입니다"
+        SymptomFlag = 1
+    else:
+        SymptomText["text"] = "증상은 이러이러 합니다"
+        SymptomFlag = 0
+
+
 #5번째 증상 및 대처
 ##########################################################################
 symptomAndhandleFrame = Frame(window,width=600,height=157,background='#FF00FF')
 symptomAndhandleFrame.pack(side='top', fill='x')
 
-SymptomHandleButton = Button(symptomAndhandleFrame,text='증상/대처법',height=5,width=20)
+SymptomHandleButton = Button(symptomAndhandleFrame,text='증상/대처법',height=5,width=20, command=SymptomHandleTextChange)
 SymptomHandleButton.grid(row=0,column=0)
 
-SymtomText = Label(symptomAndhandleFrame,text='증상/대처법', font=fontTitle,width=28,height=5,borderwidth=12,relief='ridge')
-SymtomText.grid(row=0,column=1)
+SymptomText = Label(symptomAndhandleFrame,text='증상은 이러이러 합니다', font=fontTitle,width=28,height=5,borderwidth=12,relief='ridge')
+SymptomText.grid(row=0,column=1)
+SymptomFlag = 0 # 0 = 증상, 1 = 대처
 ##########################################################################
 
 
