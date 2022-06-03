3efb0d6c12563fb06e72e355d5f7af4d8d0bcdc7
eAg302 <65862680+eAg302@users.noreply.github.com>
2022-06-03 21:37:43 +0900

    find sido test
    폴더정리, 시도 찾기 함수 테스트

---
+++ finalproject/finalproject/Center.py
@@ -0,0 +1,56 @@
+# 예방접종 센터 찾기 XML
+
+from pprint import pprint
+from xml.dom.minidom import parse
+import xml.etree.ElementTree as ET
+
+CenterDoc = None
+
+def checkDoc():
+    global CenterDoc
+    if CenterDoc == None:
+        print("Doc Empty")
+        return False
+    return True
+
+def freeDom():
+    global CenterDoc
+    if checkDoc():
+        CenterDoc.unlink()
+        print('free CenterDoc complete')
+
+
+CenterDoc = ET.parse('centers.xml')
+Ctrresults = CenterDoc.getroot()
+Ctrlst = Ctrresults.findall('data/item/col')  # 3976개의 정보
+Citemlst = Ctrresults.findall('data/item')
+#lst = results.findall('data/item')     # 284개의 센터정보
+
+cnt = 0
+
+def test(findsido):
+    global cnt
+    temp = []
+    for item in Citemlst:
+        for col in item:
+            if col.text == findsido:
+                #cnt += 1
+                #return item        #해당 아이템 찾자마자 꺼내는 코드(테스트용)
+                temp.append(item)   #해당 아이템 찾으면 리스트에 추가
+    return temp
+
+
+fitmlst = test('서울특별시')
+for fitem in fitmlst:
+    for cols in fitem.findall('col'):
+        if cols.get("name")=="facilityName":
+            print(cols.text)
+
+# #해당 item 꺼내와서 출력
+# tempcol = tempitem.findall('col')
+# for col in tempcol:
+#     print(col.text)
+
+
+#  분석하기 편하게 프리티로 확인
+#print(CenterDoc.toprettyxml(newl='\n'))
