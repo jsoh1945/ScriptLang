c82afc260875fc86a44220a11ce28886a205e410
eAg302 <65862680+eAg302@users.noreply.github.com>
2022-06-11 01:45:06 +0900

    텔레그램 연동 구현 완료

--- finalproject/finalproject/noti.py
+++ finalproject/finalproject/noti.py
@@ -4,51 +4,16 @@
 import sys
 import telepot
 from pprint import pprint # 데이터를 읽기 쉽게 출력
-from urllib.request import urlopen
 import traceback
-from xml.etree import ElementTree
-from xml.dom.minidom import parseString
 
 
 from LiveCoronaInfoJson import *
 
-key = 'W4aKgIXbfeyVPnSEdsvSTzY4hlbQsmC7xwQXeL8J3ApXRYCaPXCICZU/vwVK4GrTe6H5r/tMdTqmwbtpKQSj9A=='
+# 봇정보 : 코로나실시간정보봇 , @covid_live_info_bot
 TOKEN = '5531212925:AAGHzN7uQWvYXVNecQO72jvSyc-Hfo8JGYI'
 MAX_MSG_LENGTH = 300
-baseurl = 'http://apis.data.go.kr/1790387/covid19CurrentStatusKorea/covid19CurrentStatusKoreaJason'\
-                '&serviceKey='+key
+
 bot = telepot.Bot(TOKEN)
-
-
-# loc_param: 지역코드, date_param: yyyymm
-# 반환 : 거래 건 별로 문자열로 표현한 리스트.
-
-def getData(loc_param, date_param):
-    res_list = []
-    url = baseurl+'&LAWD_CD='+loc_param+'&DEAL_YMD='+date_param
-    res_body = urlopen(url).read()
-    strXml = res_body.decode('utf-8')
-    tree = ElementTree.fromstring(strXml)
-
-    items = tree.iter("item")  # return list type
-
-    for item in items:
-        amount = item.find("거래금액").text.strip()
-        build = item.find("건축년도").text
-        y = item.find("년").text
-        dong = item.find("법정동").text
-        apt = item.find("아파트").text
-        m = item.find("월").text
-        d = item.find("일").text
-        n = item.find("전용면적").text
-
-
-        row = y + '/' + m + '/' + d + ', ' + dong + ' ' + apt + '(' \
-              + build+') ' + n + 'm², ' + amount + '만원'
-        res_list.append(row)
-
-    return res_list
-
 
 
 def getLiveInfo(data_param):
@@ -66,10 +31,10 @@ def getLiveInfo(data_param):
         dat10death = "인구10만명당 사망률: " + LiveCoronaInfo['rate_deaths'] + "%"
         dat10newinmate = '인구 10만명당 신규입원률: ' + LiveCoronaInfo['rate_hospitalizations'] + "%"
         datcriticalrat = '재원 위중증 발생률: ' + LiveCoronaInfo['rate_severe_symptoms'] + "%"
-        row = basedate + '\n' + '\n' + datconfrim + '\n' + datenewinmate + '\n' + datcritical + '\n' + datdeath + '\n' + dat10death + '\n' + dat10newinmate + datcriticalrat
+        row = basedate + '\n' + '\n' + datconfrim + '\n' + datenewinmate + '\n' + datcritical + '\n' + datdeath + '\n' + dat10death + '\n' + dat10newinmate + '\n' + datcriticalrat
 
     elif data_param == '일일 확진자':
-        datconfrim = "일일 확진자: " + LiveCoronaInfo['cnt_confirmations']
+        datconfrim = "일일 확진자: " + LiveCoronaInfo['cnt_confirmations'] + "명"
         row = basedate + '\n' + '\n' + datconfrim + '\n'
 
     elif data_param == '일일 신규입원자':
@@ -99,7 +64,6 @@ def getLiveInfo(data_param):
     res_list.append(row)
 
     return res_list
-
 
 
 def sendMessage(user, msg):
--- finalproject/finalproject/teller.py
+++ finalproject/finalproject/teller.py
@@ -5,41 +5,16 @@ import time
 import sqlite3
 import telepot
 from pprint import pprint
-from urllib.request import urlopen
-import re
+
 from datetime import date, datetime
 
 import noti
-
-
-# date_param: 날짜, user: 사용자ID, loc_param:지역코드
-def replyAptData(date_param, user, loc_param='11710'):
-    print(user, date_param, loc_param)
-    res_list = noti.getData( loc_param, date_param )
-
-    # 하나씩 보내면 메세지 개수가 너무 많아지므로
-    # 300자까지는 하나의 메세지로 묶어서 보내기.
-    msg = ''
-    for r in res_list:
-        print( str(datetime.now()).split('.')[0], r )
-        if len(r+msg)+1>noti.MAX_MSG_LENGTH:
-            noti.sendMessage( user, msg )
-            msg = r+'\n'
-        else:
-            msg += r+'\n'
-        if msg:
-            noti.sendMessage( user, msg )
-        else:
-            noti.sendMessage( user, '%s 기간에 해당하는 데이터가 없습니다.'%date_param)
-
 
 # date_param: 날짜, user: 사용자ID, loc_param:지역코드
 def replyAptData(data_param, user):
     print(user)
     res_list = noti.getLiveInfo(data_param)
 
-    # 하나씩 보내면 메세지 개수가 너무 많아지므로
-    # 300자까지는 하나의 메세지로 묶어서 보내기.
     msg = ''
     for r in res_list:
         print( str(datetime.now()).split('.')[0], r )
@@ -50,7 +25,6 @@ def replyAptData(data_param, user):
             msg += r+'\n'
         if msg:
             noti.sendMessage( user, msg )
-
 
 def save( user, loc_param ):
     conn = sqlite3.connect('users.db')
@@ -86,41 +60,12 @@ def handle(msg):
     text = msg['text']
     args = text.split(' ')
 
-
-
     if data_param == '코로나 정보' or data_param == '일일 확진자' or data_param == '일일 신규입원자' or data_param == '일일 재원 위중증 발생자' or \
             data_param == '일일 사망자' or data_param == '인구 10만명당 사망률' or data_param == '재원 위중증 발생률':
         replyAptData(data_param, chat_id)
 
-    # if data_param == '코로나 정보':
-    #     #replyAptData(args[1], chat_id, args[2])
-    #     replyAptData(data_param, chat_id)
     else:
         noti.sendMessage(chat_id, '''모르는 명령어입니다.''')
-
-# def handle(msg):
-#     content_type, chat_type, chat_id = telepot.glance(msg)
-#     if content_type != 'text':
-#         noti.sendMessage(chat_id, '난 텍스트 이외의 메시지는 처리하지 못해요.')
-#         return
-#     text = msg['text']
-#     args = text.split(' ')
-#
-#     if text.startswith('거래') and len(args) > 1:
-#         print('try to 거래', args[1])
-#         replyAptData(args[1], chat_id, args[2])
-#     elif text.startswith('지역') and len(args) > 1:
-#         print('try to 지역', args[1])
-#         replyAptData('202205', chat_id, args[1])
-#     elif text.startswith('저장') and len(args) > 1:
-#         print('try to 저장', args[1])
-#         save(chat_id, args[1])
-#     elif text.startswith('확인'):
-#         print('try to 확인')
-#         check(chat_id)
-#     else:
-#         noti.sendMessage(chat_id, '''모르는 명령어입니다.''')
-
 
 
 today = date.today()
